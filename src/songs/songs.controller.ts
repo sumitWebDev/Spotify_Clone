330e15b530d101845e92de21b268cbd2b0fcde3f
sumitWebDev <dassumit.ece@gmail.com>
2025-07-18 20:31:29 -0600

    Song DTO

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { SongsService } from './songs.service';
+import { CreateSongDTO } from './dto/create-song-dto';
 
 @Controller('songs')
 export class SongsController {
@@ -25,8 +26,8 @@ export class SongsController {
     }
 
     @Post()
-    create(){
-       return this.songsService.create("Animals by Martin Garrix");
+    create(@Body() createSongDTO: CreateSongDTO){
+       return this.songsService.create(createSongDTO);
     }
 
 }
